63aeec4e00a02269d14b5d9adfeef029df3f4c6f
Jesus <ja3@gmail.com>
2026-05-21 20:25:01 +0200

    Proyecto Spring Boot myACW inicial

---
+++ src/main/java/com/example/myacw/MyacwApplication.java
@@ -0,0 +1,13 @@
+package com.example.myacw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyacwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyacwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myacw/MyacwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myacw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyacwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
